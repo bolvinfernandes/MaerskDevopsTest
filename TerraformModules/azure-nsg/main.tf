2ed57927e4e72789692dd453a870f37c207c96b7
bolvinfernandes <bolvinfernandes@gmail.com>
2021-06-02 19:28:35 +0100

    Update main.tf

--- TerraformModules/azure-nsg/main.tf
+++ TerraformModules/azure-nsg/main.tf
@@ -5,15 +5,16 @@ resource "azurerm_network_security_group" "nsg" {
 }
 
 resource "azurerm_network_security_rule" "nsg-rules" {
-  name                        = "test123"
-  priority                    = 100
-  direction                   = "Outbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.example.name
-  network_security_group_name = azurerm_network_security_group.example.name
+  for_each                    = local.nsgrules
+  name                        = each.key
+  priority                    = each.value.priority
+  direction                   = each.value.direction
+  access                      = each.value.access
+  protocol                    = each.value.protocol
+  source_port_range           = each.value.source_port_range
+  destination_port_range      = each.destination_port_range
+  source_address_prefix       = each.source_address_prefix
+  destination_address_prefix  = each.destination_address_prefix
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = var.network_security_group_name
 }
