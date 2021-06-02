0cf29d100198c253a6c8cb4ccde067b797075fad
bolvinfernandes <bolvinfernandes@gmail.com>
2021-06-02 19:43:13 +0100

    Update main.tf

--- TerraformModules/azure-subnet/main.tf
+++ TerraformModules/azure-subnet/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_subnet" "subnet" {
-  name                 = "${var.environment}-subnet"
+  name                 = var.environment
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.virtual_network_name
   address_prefixes     = var.address_prefixes
