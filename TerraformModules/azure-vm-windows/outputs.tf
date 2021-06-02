02fc5439292154a53f7a26b84c9e88d160968320
bolvinfernandes <bolvinfernandes@gmail.com>
2021-06-02 19:51:43 +0100

    Create outputs.tf

---
+++ TerraformModules/azure-vm-windows/outputs.tf
@@ -0,0 +1,7 @@
+output "azure-windows-vm-id" {
+  value = azurerm_windows_virtual_machine.windows-vm.id
+}
+
+output "azure-windows-vm-name" {
+  value = azurerm_windows_virtual_machine.windows-vm.name
+}
