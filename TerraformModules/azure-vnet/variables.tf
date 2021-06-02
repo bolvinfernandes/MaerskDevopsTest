7d391ff5587543a5ec0083be89fb8ba1d2f6a956
bolvinfernandes <bolvinfernandes@gmail.com>
2021-06-02 15:47:16 +0100

    Create variables.tf

---
+++ TerraformModules/azure-vnet/variables.tf
@@ -0,0 +1,8 @@
+variable "prefix" {}
+variable "env" {}
+variable "location" {}
+variable "resource_group_name" {}
+variable "address_space" {}
+variable "owner" {}
+variable "environment" {}
+variable "depends-on" {}
