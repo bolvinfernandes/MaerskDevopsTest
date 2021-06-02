7afab39f2b36c27a29b3fca5909326b41bf8cd0b
bolvinfernandes <bolvinfernandes@gmail.com>
2021-06-02 19:23:21 +0100

    Create locals.tf

---
+++ TerraformModules/azure-nsg/locals.tf
@@ -0,0 +1,29 @@
+locals { 
+nsgrules = {
+   
+    http = {
+      name                       = "http"
+      priority                   = 101
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "80"
+      source_address_prefix      = "*"
+      destination_address_prefix = "192.168.2.0/24"
+    }
+  
+    http = {
+      name                       = "https"
+      priority                   = 201
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "443"
+      source_address_prefix      = "*"
+      destination_address_prefix = "192.168.2.0/24"
+    }
+  }
+ 
+}
