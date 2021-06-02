ee9c83872d6c6b2a5851000fe989e70a7f474c51
bolvinfernandes <bolvinfernandes@gmail.com>
2021-06-02 16:23:55 +0100

    Create variables.tf

---
+++ TerraformModules/azure-storage-account/variables.tf
@@ -0,0 +1,9 @@
+variable "location" {}
+variable "name" {}
+variable "env" {}
+variable "account_replication_type" {}
+variable "account_tier" {}
+variable "account_kind" {}
+variable "resource_group_name" {}
+variable "ip_rules" {}
+variable "virtual_network_subnet_ids" {}
