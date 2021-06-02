94c47f6dbfbc9ffc3914bbe87dd3d4f20eb1a7cb
bolvinfernandes <bolvinfernandes@gmail.com>
2021-06-02 16:01:18 +0100

    Create variables.tf

---
+++ TerraformModules/azure-subnet/variables.tf
@@ -0,0 +1,6 @@
+variable "environment" {}
+variable "resource_group_name" {}
+variable "virtual_network_name" {}
+variable "address_prefixes" {}
+variable "service_endpoints" {}
+variable "depends-on" {}
